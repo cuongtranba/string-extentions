e5b1290766c5bbf93bded8047d2bf3538fcf3e1a
cuongtranba <bacuongtr@gmail.com>
2016-10-13 10:11:27 +0700

    change enviroment to windows

--- StringExtentions.py
+++ StringExtentions.py
@@ -1,14 +1,14 @@
 def SnakeCaseToCamelCase(input):
-    return input.replace("_","")
+    return input.replace("_", "")
+
 
 def CamelCaseToSnakeCase(input):
-    result=""
+    result = ""
     for char in input:
         if char.isupper():
-            result+="_"+char
+            result += "_" + char
         else:
-            result+=char
-    if result[0]=="_":
+            result += char
+    if result[0] == "_":
         result = result[1:]
     return result
-
--- tests.py
+++ tests.py
@@ -1,14 +1,19 @@
 import unittest
 import StringExtentions
 
-exampleCamelCase="ToSnakeCase"
-exampleSnakeCase="To_Camel_Case"
+exampleCamelCase = "ToSnakeCase"
+exampleSnakeCase = "To_Camel_Case"
+
 
 class TestStringExtentions(unittest.TestCase):
+
     def test_snakecase_to_camelcase(self):
-        self.assertEqual(StringExtentions.SnakeCaseToCamelCase(exampleSnakeCase),"ToCamelCase")
+        self.assertEqual(StringExtentions.SnakeCaseToCamelCase(
+            exampleSnakeCase), "ToCamelCase")
+
     def test_camelcase_to_snakecase(self):
-        self.assertEqual(StringExtentions.CamelCaseToSnakeCase(exampleCamelCase),"To_Snake_Case")
+        self.assertEqual(StringExtentions.CamelCaseToSnakeCase(
+            exampleCamelCase), "To_Snake_Case")
 
 if __name__ == '__main__':
     unittest.main()
